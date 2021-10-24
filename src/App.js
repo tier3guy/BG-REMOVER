1709158e6e71eed569e15bc9bb17c960fe786406
Avinash Gupta <73595465+tier3guy@users.noreply.github.com>
2021-10-24 14:05:30 +0530

    IMPORTING Partials

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './Components/JSX/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
     </div>
   );
 }
---
+++ src/Components/JSX/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../CSS/Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Remove BG</h1>
+    </nav>
+  )
+}
+
+export default Navbar;
