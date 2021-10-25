429571f5068ad730473f0178413fb62f68c958a8
Avinash Gupta <73595465+tier3guy@users.noreply.github.com>
2021-10-25 15:33:28 +0530

    BLOCKING DEFAULTS

--- src/API/Api.js
+++ src/API/Api.js
@@ -5,3 +5,23 @@ const API_KEY = process.env.REACT_APP_BG_REMOVER_API_KEY;
 const axios = require('axios');
 
 
+// API Calling function
+export const getRemovedBG = (image) => {
+  axios({
+    method : 'post',
+    url : '',
+    data : image,
+    responseType : 'arraybuffer',
+    headers : {
+      ...image.getHeaders(),
+      'X-Api-Key' : API_KEY
+    },
+    encoding : null
+  })
+  .then(
+    () => console.log('BG REMOVAL SUCCESS')
+  )
+  .catch(
+    (error) => console.log(error)
+  )
+}
--- src/Components/JSX/DragableContainer.jsx
+++ src/Components/JSX/DragableContainer.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import '../CSS/DragableContainer.css'
+import React from 'react';
+import '../CSS/DragableContainer.css';
+
 
 const DragableContainer = () => {
+  
+  // Creating a fake input over the 'Browse Files Button'
+  const ImageFile = document.createElement('input');
+  ImageFile.type = 'file';
+  ImageFile.accept = 'image/png, image/jpeg, image/jpg';
+  
+  const BlockDefault = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }
   return (
-    <div className = "dragable-container">
+    <div className = "dragable-container"
+         onDragOver = {(event) => {BlockDefault(event)}}
+         onDrag = {(event) => {BlockDefault(event)}}
+         onDragEnter = {(event) => {BlockDefault(event)}}
+         onDrop = {(event) => {BlockDefault(event)}}>
+         
       <p className = "font-light">Drag and Drop your File Here</p>
       <p className = "font-light">OR</p>
-      <button className = "browse-button">Browse Filse</button>
+      <button className = "browse-button" id = "browse-btn" onClick = {() => ImageFile.click()}>
+        Browse Files
+      </button>
     </div>
   )
 }
